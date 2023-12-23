56add83a93ecb1f73faff88f7a44315981412fdf
Inj3ctor <108236056+kok32e423@users.noreply.github.com>
2023-12-23 21:11:06 +0200

    Update client.js

--- client.js
+++ client.js
@@ -19,42 +19,39 @@ function HintRoom(hint){
   ui.Hint.Value = hint;
 }
 // добавляем команды
-var blueTeam = addteam('blue', 'синие', { b: 0.7 }, 1);
-var redTeam = addteam('red', 'красные', { r: 0.7 }, 2);
+var blueTeam = addteam('blue', '<b>синие</b>', { b: 0.7 }, 1);
+var redTeam = addteam('red', '<b>красные</b>', { r: 0.7 }, 2);
 // константы
 const prop = Properties.GetContext();
 const roomHp = 35;
+const needPlayers = 2;
 const roomInventory = Inventory.GetContext(); 
 const ui = Ui.GetContext(); 
 const stateProp = prop.Get("State");
-const maxKills = 45;
+const maxRounds = 10;
 const roomContextedProp = contextedProperties.GetContext();
 const spawn = Spawns.GetContext();
 const gameTimer = Timers.GetContext().Get('gtim');
-const banned = '596D1288BD7F8CF7/C002224F3666744D/';
+const waitingTimer = Timers.GetContext().Get('wtim');
+const roundWinTeamIdProp = prop.Get("rWinTeam");
 // параметры
 ui.MainTimerId.Value = gameTimer.Id;
 TeamsBalancer.IsAutoBalance = true;
 prop.GameModeName.Value = "GameModes/Knife";
-spawn.RespawnTime.Value = 3;
-BreackGraph.WeakBlocks = GameMode.Parameters.GetBool('LoosenBlocks');
-Map.Rotation = GameMode.Parameters.GetBool("MapRotation");
 // переменные
-var gameWinner = 'ничья!';
-var gameTime = 525;
-var restartTime = 15;
+var gameWinner = 'draw!';
+var gameTime = 180;
 // инициализация значений команд
 function initTeams(){
-for(e = Teams.GetEnumerator();e.MoveNext();){
+var e = Teams.GetEnumerator();
+     while (e.moveNext()){
      let teamProp = Properties.GetContext(e.Current);
+     // раунды
+     teamProp.Get('winRounds').Value = 0;
      // убийства команд
      teamProp.Get('teamKills').Value = 0;
      }
 }    
-// поиск в строке
-function src(str, el) {
-    return str.search(el);
-}
 // события
 Teams.OnPlayerChangeTeam.Add(function(player){
 player.Spawns.Spawn();
@@ -62,80 +59,44 @@ player.Spawns.Spawn();
 Teams.OnRequestJoinTeam.Add(function(player,team){
 team.Add(player);
 });
-// чото
+// изменение значений
 Properties.OnTeamProperty.Add(function(context, value){
-let contextTProp = context.Team.Properties;
-contextTProp.Get('colorK').Value = '<color='+context.Team.Id+'>'+contextTProp.Get('teamKills').Value+'</color>';
-if(value.Name !== 'teamKills')return;
-if(value.Value >= maxKills)endOfGame(); return;
-}); 
-// игрок выходит
-Players.OnPlayerDisconnected.Add(function(player){
-if(stateProp.Value !== 'gameState')return;
-save(player);
+  let contextTeamProp = context.Team.Properties;
+  if(value.Name === 'winRounds' || value.Name === 'teamKills')contextTeamProp.Get('info').Value = 'wRounds: ' + contextTeamProp.Get('winRounds').Value + '\n' + 'Kills: ' + contextTeamProp.Get('teamKills').Value;
+  if(contextTeamProp.Get('winRounds').Value >= maxRounds && value.Name == 'winRounds') endOfGame();
 });
-// игрок заходит
-Players.OnPlayerConnected.Add(function(player){
-player.Ui.Hint.Value = player + ' чья команда наберет больше очков, та и победит!';
-if(stateProp.Value !== 'gameState')return;
-load(player);
-  if (src(banned, p.id) >= 0) ban(p);
-      // функция бана
-      function ban(p)
-    {
-        p.Spawns.Enable = false;
-        blueTeam.Add(p);
-        p.Ui.Hint.Value = 'ты забанен.'
-        p.Spawns.Despawn();    
-    } 
-});
-// сохр
-function save(player){ 
-  let propPlr = player.Properties;
-  prop.Get(player.id + 'Kills').Value = propPlr.Kills.Value;
-  prop.Get(player.id + 'Deaths').Value = propPlr.Deaths.Value;
-}
-// загрузка сохр
-function load(player){
-  let propPlr = player.Properties;
-  propPlr.Kills.Value = prop.Get(player.id + 'Kills').Value; 
-  propPlr.Deaths.Value = prop.Get(player.id + 'Deaths').Value;
-}
-// выводим киллы наверху
-TeamProp('red', 'blue', 'colorK');
 // делаем щит при спавне
 spawn.OnSpawn.Add(function(player){
   player.Properties.Immortality.Value=true;
   player.Timers.Get('immo').Restart(3);
 });
 Timers.OnPlayerTimer.Add(function(timer){
-  if(timer.Id!= 'immo')return;
+  if(timer.Id!= 'immo') return;
   timer.Player.Properties.Immortality.Value=false;
 });
 Damage.OnKill.Add(function(player, killed){
-if(killed.Team == player.Team)return;
+if(killed.Team == player.Team) return;
   player.Properties.Get('Kills').Value++;
   player.Team.Properties.Get('teamKills').Value++;
 });
 Damage.OnDeath.Add(function(player){
   player.Properties.Get('Deaths').Value++;
 });
-Damage.OnDamage.Add(function(player1, player2, damage){
-if(player2.Team == player1.Team) return;
-player1.Properties.Scores.Value += Math.floor(damage);
-});
+// sf
+Damage.OnDeath.Add(getRoundWinner);
+Players.OnPlayerDisconnected.Add(getRoundWinner);
 // значения в лидерборде
 LeaderBoard.PlayerLeaderBoardValues = [
  { Value: 'Kills', ShortDisplayName: 'к' },
- { Value: 'Deaths', ShortDisplayName: 'с' }
-];
+ { Value: 'Deaths', ShortDisplayName: 'с' }];
 //смена состояний
 function switchStates(){
+let players = Players.Count;
 switch (stateProp.Value){
     case 'gameState': 
-         endOfGame();
+         endOfRound();
          break;
-    case 'endOfGame': 
+    case 'endOfRoundState': 
          gameMode();
          break;
        }
@@ -146,56 +107,93 @@ initTeams();
 gameMode();
 function gameMode(){
   stateProp.Value = 'gameState'; 
-     spawn.Enable = true;
+     HintRoom('убейте всех врагов, чтобы выиграть раунд!');
+     // выводим инфу наверху
+     TeamProp('red', 'blue', 'info');
+     // запрещяем респавн
+     spawn.RespawnEnable = false;
      // despawn
      spawn.Despawn();
+     spawn.Enable = true;
 	 SpawnTeams();
-	 gameTimer.Restart(gameTime); 
+	 gameTimer.Restart(115); // 115
+}
+
+function endOfRound(teamGetWinProp){
+  stateProp.Value = 'endOfRoundState';
+    var teamGetWinProp = Teams.Get(roundWinTeamIdProp.Value);   
+    if(teamGetWinProp !== null){
+    HintRoom('раунд закончен, ' + teamGetWinProp + ' wins!');
+    // прибавляем winRounds
+    var winProp = teamGetWinProp.Properties.Get('winRounds');
+    if(winProp.Value == null) winProp.Value = 1;
+    else winProp.Value = winProp.Value + 1;
+ }
+  else HintRoom('раунд закончен, draw!'); 
+    // запрещаем спавны
+    spawn.Enable = false;
+    gameTimer.Restart(10); 
 }
 
 function endOfGame(){
   stateProp.Value = 'endOfGameState';
-    hintReset();
-    Damage.GetContext().DamageIn.Value = false;
     // определяем победителя игры
-    if(blueTeam.Properties.Get('teamKills').Value > redTeam.Properties.Get('teamKills').Value)gameWinner = 'синие побеждают!';
-    if(redTeam.Properties.Get('teamKills').Value > blueTeam.Properties.Get('teamKills').Value)gameWinner = 'красные побеждают!';
+    if(blueTeam.Properties.Get('winRounds').Value > redTeam.Properties.Get('winRounds').Value)gameWinner = 'blue Team wins!';
+    if(redTeam.Properties.Get('winRounds').Value > blueTeam.Properties.Get('winRounds').Value)gameWinner = 'red Team wins!';
     // выводим в подсказки
-    HintRoom('игра закончена. ' + gameWinner);
+    HintRoom('игра закончена, ' + gameWinner);
+    spawn.Enable = false;
     // вешаем рестарт
     gameTimer.OnTimer.Add(restartGame);
-    gameTimer.Restart(restartTime); 
+    gameTimer.Restart(10); 
 }
 // инвентарь в комнате
 roomInventory.Main.Value = false;
 roomInventory.Secondary.Value = false;
-if (GameMode.Parameters.GetBool('IsExplosive')) {
-     roomInventory.ExplosiveInfinity.Value = true;
-     roomInventory.Explosive.Value = true;
-} else {
-	 roomInventory.ExplosiveInfinity.Value = true;
-}
+roomInventory.Explosive.Value = false;
 roomInventory.Build.Value = false;
 // хп в комнате
 roomContextedProp.MaxHp.Value = roomHp;
-// спавн команд
+// spawntm
 function SpawnTeams(){
 	var e = Teams.GetEnumerator();
 	    while (e.moveNext()){
 		Spawns.GetContext(e.Current).Spawn();
 	 }
 }
-// рестарт
+// res
 function restartGame(){
-	Game.RestartGame(); 
+	Game.RestartGame();
 }
-//убираем хинты
-function hintReset(){
-for(e= Players.GetEnumerator();e.MoveNext();)e.Current.ui.Hint.Reset();
+// определяем победителя
+function getRoundWinner(){
+roundWinnerTeam = null;
+wins = 0;
+alifeCount = 0;
+hasEmptyTeam = false;
+if(stateProp.value !== 'gameState')return;
+for (e = Teams.GetEnumerator(); e.moveNext();){
+      let teamProp = Properties.GetContext(e.Current);
+            // хз чо ето 
+            alifeCount += e.Current.GetAlivePlayersCount();
+            if(e.Current.GetAlivePlayersCount() > 0){ 
+            // эсли в команде есть живые, прибавляем wins
+            ++wins; 
+            roundWinnerTeam = e.Current;
+         }
+            if(e.Current.Count == 0) hasEmptyTeam = true;
+         }
+          if(!hasEmptyTeam && alifeCount > 0 && wins === 1){
+		     roundWinTeamIdProp.Value = roundWinnerTeam.Id;
+		     endOfRound(roundWinnerTeam);
+		     return;
+	      }
+	       if(alifeCount == 0){
+		     roundWinTeamIdProp.Value = null;
+		     endOfRound(null);
+	      }
+	       if(!gameTimer.IsStarted){
+		     roundWinTeamIdProp.Value = null;
+		     endOfRound(null);
+	      }
 }
-// пон
-for(e=Players.GetEnumerator();e.MoveNext();)e.Current.ui.Hint.Value= e.Current + ' чья команда наберет больше очков, та и победит!';
-
-
-
-	
