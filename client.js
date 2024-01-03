89709c424561be7315208b0e8cc98ae29a7d80d3
Inj3ctor <108236056+kok32e423@users.noreply.github.com>
2024-01-03 08:23:23 +0200

    Update client.js

--- client.js
+++ client.js
@@ -95,7 +95,7 @@ Teams.OnPlayerChangeTeam.Add(function (p)
 Properties.OnPlayerProperty.Add(function (c, v) 
 {
    let p = c.Player;
-   p.Team.Properties.Get('info').Value = 'звание: говноед\nwins: ' + p.Team.Properties.Get('wins').Value + ', kills: ' + p.Team.Properties.Get('kills').Value;
+   p.Team.Properties.Get('info').Value = 'rank: поопо, wins: ' + p.Team.Properties.Get('wins').Value + ', kills: ' + p.Team.Properties.Get('kills').Value;
 });
 
 Players.OnPlayerConnected.Add(function (p)
@@ -169,8 +169,8 @@ const End = function (team)
 {
    s.Value = 'end';
    if (team != null) 
-  {
-  	 team.Properties.Get('wins').Value += 1;
+   {
+  	 tm.Properties.Get('wins').Value += 1;
        let e = Players.GetEnumerator ();
        while (e.MoveNext ()) if (e.Current.Team == team) e.Current.Properties.Get('Scores').Value += 1;
    }
