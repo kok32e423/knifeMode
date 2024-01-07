a816e66593bdccf41f813a78599a39b3f91f50af
Inj3ctor <108236056+kok32e423@users.noreply.github.com>
2024-01-07 15:34:25 +0200

    Update client.js

--- client.js
+++ client.js
@@ -95,7 +95,7 @@ Properties.OnPlayerProperty.Add(function (c, v)
 {
    let p = c.Player,
    nam = v.Name;
-   if (nam != 'info1') p.Team.Properties.Get('info1').Value = ' Звание: новичёк ' + n + '---------------------' + n;
+   if (nam != 'info1') p.Team.Properties.Get('info1').Value = '.:: Звание: новичёк ::.' + n + '-----------------------' + n;
 });
 
 Players.OnPlayerConnected.Add(function (p)
