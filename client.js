d9117b6ad9fb4ed7a59cca2f7c33b016227dd7ec
Inj3ctor <108236056+kok32e423@users.noreply.github.com>
2024-01-07 16:58:12 +0200

    Update client.js

--- client.js
+++ client.js
@@ -93,9 +93,9 @@ Teams.OnPlayerChangeTeam.Add(function (p)
 
 Properties.OnPlayerProperty.Add(function (c, v) 
 {
-   let p = c.Player,
-   nam = v.Name;
-   if (nam != 'info1') p.Team.Properties.Get('info1').Value = '  Звание: новичёк  ' + n + '' + n + '   level: 1, exp: 0 <size=58.5>/ 25</size>  ';//------------------------
+   let p = c.Player, 
+   name = v.Name;
+   if (name != 'info1') p.Team.Properties.Get('info1').Value = '<color=#FFFFFF>  Звание: новичёк  ' + n + '' + n + '   level: 1, exp: 0 <size=58.5>/ 25</size></color>  ';//------------------------
 });
 
 Players.OnPlayerConnected.Add(function (p)
