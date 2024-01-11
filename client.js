2abaf20c790279e9bdb56b5225a646cf5d9b7aeb
Inj3ctor <108236056+kok32e423@users.noreply.github.com>
2024-01-11 13:43:01 +0200

    Update client.js

--- client.js
+++ client.js
@@ -126,7 +126,6 @@ try {
         {
             Update (p);
             p.Properties.Get('Deaths').Value += 1;
-            p.Properties.Get('experience').Value += 25;
         }); 
         
         Damage.OnKill.Add(function (p, vic) 
