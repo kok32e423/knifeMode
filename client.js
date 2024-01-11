c7eea0d7cb9c484e6da4b42758096e854b0f0f1b
Inj3ctor <108236056+kok32e423@users.noreply.github.com>
2024-01-11 10:51:10 +0200

    Update client.js

--- client.js
+++ client.js
@@ -94,11 +94,11 @@ try {
          Properties.OnPlayerProperty.Add (function (context, e) 
          {
              let p = context.Player;
-             p.Team.Properties.Get(p.Id + 'info1').Value = '<color=#FFFFFF>  Звание: ' + prop.Get(p.Id + 'rank').Value + '  ' + n + '' + n + '   level: ' + prop.Get(p.Id + 'level').Value + ', exp: ' + prop.Get(p.Id + 'experience').Value + ' <size=58.5>/ ' + prop.Get(p.Id + 'next').Value  + '</size></color>  ';
-             if (prop.Get(p.Id + 'experience').Value < prop.Get(p.Id + 'next').Value) return;
-             prop.Get(p.Id + 'level').Value += 1;
-             prop.Get(p.Id + 'next').Value = RANKS[prop.Get(p.Id + 'level').Value - 1].exp;
-             prop.Get(p.Id + 'rank').Value = RANKS[prop.Get(p.Id + 'level').Value - 2].name; 
+             p.Team.Properties.Get(p.Id + 'info1').Value = '<color=#FFFFFF>  Звание: ' + p.Properties.Get('rank').Value + '  ' + n + '' + n + '   level: ' + p.Properties.Get('level').Value + ', exp: ' + p.Properties.Get('experience').Value + ' <size=58.5>/ ' + p.Properties.Get('next').Value  + '</size></color>  ';
+             if (e.Name != 'experience' && e.Value < p.Properties.Get('next').Value) return;
+             p.Properties.Get('level').Value += 1;
+             p.Properties.Get('next').Value = RANKS[p.Properties.Get('level').Value - 1].exp;
+             p.Properties.Get('rank').Value = RANKS[p.Properties.Get('level').Value - 2].name; 
          });
          
          Properties.OnTeamProperty.Add (function (context, e) 
@@ -122,6 +122,7 @@ try {
         {
             p.Properties.Get('Immortality').Value = true;
             p.Timers.Get('Im').Restart (3);
+            p.Ui.Hint.Reset ();
         });
         
         Damage.OnDeath.Add(function (p) 
@@ -135,20 +136,22 @@ try {
            if (vic.Team == p.Team) return;
            pos = p.PositionIndex.x - vic.PositionIndex.x + p.PositionIndex.y - vic.PositionIndex.y + p.PositionIndex.z - vic.PositionIndex.z;
            if (pos != 0) vic.Ui.Hint.Value = p.NickName + ' убил вас с расстояния ' + Math.abs (pos) + ' блоков!';
-           p.Properties.Get('Kills').Value += 1;  
-           prop.Get(p.Id + 'experience').Value += Rand (2, 8);
+           p.Properties.Get('Kills').Value += 1, prop.Get(p.Id + 'experience').Value += Rand (2, 8);
        });  
 
        Players.OnPlayerDisconnected.Add(function (p) 
        {   
           Update (p); 
+          P_PROPERTIES.NAMES.forEach(function (name) { 
+              prop.Get(p.Id + name).Value = p.Properties.Get(name).Value;
+          });
        }); 
 
        Players.OnPlayerConnected.Add(function (p) 
        { 
           P_PROPERTIES.NAMES.forEach(function (name, el) { 
-            if (prop.Get(p.Id + name).Value == null) prop.Get(p.Id + name).Value = P_PROPERTIES.VALUES[el];
-         });
+              prop.Get(p.Id + name).Value == null ? p.Properties.Get(name).Value = P_PROPERTIES.VALUES[el] : p.Properties.Get(name).Value = prop.Get(p.Id + name).Value;
+          });
        }); 
     
          main.OnTimer.Add (function () {
@@ -185,7 +188,7 @@ try {
          });
          
          P_PROPERTIES.NAMES.forEach(function (name, el) { 
-            for (let e = Players.GetEnumerator (); e.MoveNext();) prop.Get(e.Current.Id + name).Value = P_PROPERTIES.VALUES[el];
+            for (let e = Players.GetEnumerator (); e.MoveNext();) p.Properties.Get(name).Value = P_PROPERTIES.VALUES[el];
          });
          
          BreackGraph.Damage = false, ['Main', 'Secondary', 'Explosive', 'Build'].forEach(function (el) { Inventory.GetContext()[el].Value = false; });
