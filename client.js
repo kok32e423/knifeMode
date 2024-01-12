1751d9fa68057b43af56858e6c7239d540f3aa6c
Inj3ctor <108236056+kok32e423@users.noreply.github.com>
2024-01-12 15:53:03 +0200

    Update client.js

--- client.js
+++ client.js
@@ -105,7 +105,7 @@ try {
                 level = p.Team.Properties.Get (p.Id + 'level').Value, rank = p.Team.Properties.Get (p.Id + 'rank').Value, next = p.Team.Properties.Get (p.Id + 'next').Value,
              experience = p.Team.Properties.Get (p.Id + 'experience').Value;   
              
-             p.Team.Properties.Get(p.Id + 'info1').Value = '<color=#FFFFFF>  Звание: ' + String(rank) + '  ' + n + '' + n + '   level: ' + String(level) + ', exp: ' + String(experience) + ' <size=58.5>/ ' + String(next) + '</size></color>  ';
+             p.Properties.Get(p.Id + 'info1').Value = '<color=#FFFFFF>  Звание: ' + String(rank) + '  ' + n + '' + n + '   level: ' + String(level) + ', exp: ' + String(experience) + ' <size=58.5>/ ' + String(next) + '</size></color>  ';
          });
     
          Timers.OnPlayerTimer.Add (function (t) 
