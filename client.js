596de3cad1ed31adbe2c561b5021dfbb3ec9202d
Inj3ctor <108236056+kok32e423@users.noreply.github.com>
2024-01-15 18:57:27 +0200

    Update client.js

--- client.js
+++ client.js
@@ -1,18 +1,24 @@
 try {
 	
 	
-            const n = '\n', PROPERTIES = { NAMES: ['wins', 'looses'], VALUES: [0, 0] }, RANKS = [
-                      { name: 'новичёк', exp: 25 },         
-                      { name: 'черпак', exp: 40 },         
-                      { name: 'каструля', exp: 65 },
-                      { name: 'мастер', exp: 85 },
-                      { name: 'говноед', exp: 115 },
-                      { name: 'stormtro', exp: 140 },
-                      { name: 'lololoshk', exp: 160 },
-                      { name: 'странник', exp: 185 },
-                      { name: 'босс', exp: 1000 }
-            ], P_PROPERTIES = { NAMES: ['next', 'experience', 'level', 'rank'], VALUES: [RANKS[0].exp, 0, 1, RANKS[0].name] }, props = Properties.GetContext(), state = props.Get('state'), last_round = props.Get('lzt_round'), main = Timers.GetContext().Get('main'), ui = Ui.GetContext(), spawn = Spawns.GetContext(), con_prop = contextedProperties.GetContext(), BLACKLIST = 'C002224F3666744D|596D1288BD7F8CF7|C925816BE50844A9|9B94CBC25664BD6D|2F665AF97FA6F0EF|E24BE3448F7DF371|CBCE0678C099C56E', ADMIN_ID = 'EC76560AA6B5750B';
-         
+            const n = '\n', PROPERTIES = [
+               { name: ['wins', 'looses'], value: [0, 0] }, 
+               { name: ['next', 'experience', 'level', 'rank'], value: [RANKS[0].exp, 0, 1, RANKS[0].name] }
+            ], RANKS =
+             [
+               { name: 'новичёк', target: 25 },         
+               { name: 'черпак', target: 40 },         
+               { name: 'каструля', target: 65 },
+               { name: 'мастер', target: 85 },
+               { name: 'говноед', target: 115 },
+               { name: 'stormtro', target: 140 },
+               { name: 'lololoshk', target: 160 },
+               { name: 'странник', target: 185 },
+               { name: 'босс', target: 1000 } 
+             ],
+             props = Properties.GetContext(), state = props.Get('state'), last_round = props.Get('lzt_round'), main = Timers.GetContext().Get('main'), ui = Ui.GetContext(), spawn = Spawns.GetContext(), con_prop = contextedProperties.GetContext(), BLACKLIST = 'C002224F3666744D|596D1288BD7F8CF7|C925816BE50844A9|9B94CBC25664BD6D|2F665AF97FA6F0EF|E24BE3448F7DF371|CBCE0678C099C56E', ADMIN_ID = 'EC76560AA6B5750B';
+          
+            
             const _Add = function (tag, name, color, spawn) {    	
                       let team = Teams.Get (tag);
                       Teams.Add (tag, '<b><size=22>' + name.up.substring(0, 1) + '</size><size=17>' + name.up.substring(1) + '</size></b>' + n + '<size=17>' + name.down.substring(0, 1) + '</size>' + name.down.substring(1), _Color (color));
@@ -56,32 +62,29 @@ try {
             const _Another = function (t) {
                     if (t == blue) return red;
                     else return 
-                            blue;
+                               blue;
             }
             
-            spawn.RespawnEnable = false, BreackGraph.Damage = false, ui.MainTimerId.Value = main.Id;  
-         
             LeaderBoard.PlayerLeaderBoardValues = [
                    { Value: 'Kills', ShortDisplayName: '<size=11.9><b>ᴋ</b></size>' },
                    { Value: 'Deaths', ShortDisplayName: '<size=11.9><b>ᴅ</b></size>' }
             ];
-            
+                      
+            spawn.RespawnEnable = false, BreackGraph.Damage = false, ui.MainTimerId.Value = main.Id;  
+         
             const blue = _Add ('blue', { up: 'спецназовцы ᵏⁿⁱᶠᵉᵉ', down: '' }, '#476AEC', 1),
             red = _Add ('red', { up: 'террористы ᵏⁿⁱᶠᵉᵉ', down: '' }, '#FE5757', 2);
             
-            P_PROPERTIES.NAMES.forEach (function (name, index) { for (let e = Players.GetEnumerator(); e.MoveNext();) props.Get(e.Current.Id + name).Value = P_PROPERTIES.VALUES[index]; });
+            PROPERTIES[1].name.forEach (function (el1, el2) { for (let e = Players.GetEnumerator (); e.MoveNext();) props.Get(e.Current.Id + el1).Value = P_PROPERTIES[1].value[el2]; });
+            PROPERTIES[0].name.forEach (function (el1, el2) { for (let e = Teams.GetEnumerator (); e.MoveNext();) e.Current.Properties.Get(el1).Value = P_PROPERTIES[0].value[el2]; });
             
             Teams.OnRequestJoinTeam.Add (function (p, t) {
                    if (state.Value == 'end' || _Found (BLACKLIST, p.Id, '|')) return;
                    t.Add (p);  
             });
-               
-            Teams.OnPlayerChangeTeam.Add (function (p) { p.Spawns.Spawn (), p.Ui.TeamProp2.Value = { Team: p.Team.Id, Prop: p.Id + 'info1' }, p.Team.Properties.Get(p.Id + 'info1').Value = '<color=#FFFFFF>  Звание: ' + String(props.Get(p.Id + 'rank').Value) + '  ' + n + '' + n + '   level: ' + String(props.Get(p.Id + 'level').Value) + ', exp: ' + String(props.Get(p.Id + 'experience').Value) + ' <size=58.5>/ ' + String(props.Get(p.Id + 'next').Value) + '</size></color>  '; });     
-            
-            Teams.OnAddTeam.Add (function (t) { 
-            	   PROPERTIES.NAMES.forEach (function (name, index) { t.Properties.Get(name).Value = PROPERTIES.VALUES[index]; });
-                   t.Ui.TeamProp1.Value = { Team: t.Id, Prop: 'info2' };               
-            });
+                
+            Teams.OnPlayerChangeTeam.Add (function (p) { p.Spawns.Spawn (), p.Ui.TeamProp2.Value = { Team: p.Team.Id, Prop: p.Id + 'info1' }, p.Team.Properties.Get(p.Id + 'info1').Value = '<color=#FFFFFF>  Звание: ' + String(props.Get(p.Id + 'rank').Value) + '  ' + n + '' + n + '   level: ' + String(props.Get(p.Id + 'level').Value) + ', exp: ' + String(props.Get(p.Id + 'experience').Value) + ' <size=58.5>/ ' + String(props.Get(p.Id + 'next').Value) + '</size></color>  '; });      
+            Teams.OnAddTeam.Add (function (t) { t.Ui.TeamProp1.Value = { Team: t.Id, Prop: 'info2' }; });
                      
             Properties.OnTeamProperty.Add (function (context, e) {
                    let t = context.Team;
@@ -145,9 +148,9 @@ try {
  
             const _End = function (team) { 
                    state.Value = 'end', main.Restart (10);          
-                  /* if (team == null) return;
+                   if (team == null) return;
                    for (let e = Players.GetEnumerator(); e.MoveNext();) if (e.Current.Team == team) e.Current.Properties.Get('Scores').Value += 1;
-                   team.Properties.Get('wins').Value += 1, Another(team).Properties.Get('looses').Value += 1;      */
+                   team.Properties.Get('wins').Value += 1, Another(team).Properties.Get('looses').Value += 1;      
             } 
              
             ['Main', 'Secondary', 'Explosive', 'Build'].forEach (function (name) { Inventory.GetContext()[name].Value = false; });
