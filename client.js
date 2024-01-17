74efc69c8249b3015fce7312cf05274b3e837af1
Inj3ctor <108236056+kok32e423@users.noreply.github.com>
2024-01-17 18:49:35 +0200

    Update client.js

--- client.js
+++ client.js
@@ -128,7 +128,7 @@ try {
             }); 
                          
             Damage.OnDeath.Add (function (p) {
-            	  _Update ();
+            	  _Update (p);
                   p.Properties.Get('Deaths').Value += 1;
             }); 
                       
@@ -144,7 +144,7 @@ try {
             });  
           
             Players.OnPlayerConnected.Add (function (p) { PROPERTIES[1].name.forEach(function (element1, element2) { if (props.Get(p.Id + element1).Value == null) props.Get(p.Id + element1).Value = PROPERTIES[1].value[element2]; }); });                          
-            Players.OnPlayerDisconnected.Add (function (p) { _Update (), p.Team.Properties.Get(p.Id + 'info1').Value = null; });                          
+            Players.OnPlayerDisconnected.Add (function (p) { _Update (p), p.Team.Properties.Get(p.Id + 'info1').Value = null; });                          
                    
             inv.Main.Value = false;
             inv.Secondary.Value = false;
