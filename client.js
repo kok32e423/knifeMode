42772cee1794164ebffac8b181be64bca64d8fec
Inj3ctor <108236056+kok32e423@users.noreply.github.com>
2024-01-17 18:11:25 +0200

    Update client.js

--- client.js
+++ client.js
@@ -42,10 +42,14 @@ try {
                  }  
             }
           
-            const _Update = function (p) {
-                	if (s.Value != 'game') return;
-                    if (p.Team.GetAlivePlayersCount() == 0 && _Another(p.Team).GetAlivePlayersCount() > p.Team.GetAlivePlayersCount()) return _End (_Another(p.Team));
-                    if (p.Team.GetAlivePlayersCount() == 0 && _Another(p.Team).GetAlivePlayersCount() == 0) return _End (null);
+            const _Update = function () {
+                	if (s.Value == 'game') {
+               	    for (let e = Players.GetEnumerator (); e.MoveNext();) {
+                       const p = e.Current.Team;       
+                            if (p.GetAlivePlayersCount() <= 0 && _Another (p).GetAlivePlayersCount() > p.GetAlivePlayersCount()) return _End (_Another(p));
+                            if (p.GetAlivePlayersCount() <= 0 && _Another (p).GetAlivePlayersCount() <= 0) return _End (null);
+                      }
+                 }
             }
                 
             const _Spawn = function () { 
@@ -68,7 +72,7 @@ try {
             }
                       
             const _Game = function () {
-                   s.Value = 'game', _Spawn (), main.Restart (4); 
+                   s.Value = 'game', _Spawn (), main.Restart (115); 
                    _Text (n + 'round: ' + last_round.Value);
             }   
  
@@ -127,7 +131,7 @@ try {
             }); 
                          
             Damage.OnDeath.Add (function (p) {
-            	  _Update (p);
+            	  _Update ();
                   p.Properties.Get('Deaths').Value += 1;
             }); 
                       
@@ -143,7 +147,7 @@ try {
             });  
           
             Players.OnPlayerConnected.Add (function (p) { PROPERTIES[1].name.forEach(function (element1, element2) { if (props.Get(p.Id + element1).Value == null) props.Get(p.Id + element1).Value = PROPERTIES[1].value[element2]; }); });                          
-            Players.OnPlayerDisconnected.Add (function (p) { p.Team.Properties.Get(p.Id + 'info1').Value = null; });                          
+            Players.OnPlayerDisconnected.Add (function (p) { _Update (), p.Team.Properties.Get(p.Id + 'info1').Value = null; });                          
                    
             inv.Main.Value = false;
             inv.Secondary.Value = false;
